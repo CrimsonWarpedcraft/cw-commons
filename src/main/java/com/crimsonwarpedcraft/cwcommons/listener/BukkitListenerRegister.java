3399c494360e3c8ee7e394d4468ce9fb803b9388
Levi Muniz <levi.muniz17@gmail.com>
2021-07-06 16:25:04 -0600

    Added BukkitListenerManager

---
+++ src/main/java/com/crimsonwarpedcraft/cwcommons/listener/BukkitListenerRegister.java
@@ -0,0 +1,43 @@
+package com.crimsonwarpedcraft.cwcommons.listener;
+
+import java.util.Objects;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+
+/**
+ * Registers event listeners with the server.
+ *
+ * @author Copyright (c) Levi Muniz. All Rights Reserved.
+ */
+public class BukkitListenerRegister {
+  private final Plugin plugin;
+  private final PluginManager manager;
+
+  private BukkitListenerRegister(Plugin plugin) {
+    this.plugin = plugin;
+    this.manager = plugin.getServer().getPluginManager();
+  }
+
+  /**
+   * Registers an event listener with the server.
+   *
+   * @param listener the event listener to register
+   * @return this instance that can be used for method chaining
+   */
+  public BukkitListenerRegister registerListener(Listener listener) {
+    manager.registerEvents(Objects.requireNonNull(listener), plugin);
+
+    return this;
+  }
+
+  /**
+   * Constructs and returns a ListenerRegister object.
+   *
+   * @param plugin the plugin that is registering the event listeners
+   * @return a ListenerRegister object
+   */
+  public static BukkitListenerRegister getNewBukkitListenerRegister(Plugin plugin) {
+    return new BukkitListenerRegister(Objects.requireNonNull(plugin));
+  }
+}
---
+++ src/test/java/com/crimsonwarpedcraft/cwcommons/listener/BukkitListenerRegisterTest.java
@@ -0,0 +1,49 @@
+package com.crimsonwarpedcraft.cwcommons.listener;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.crimsonwarpedcraft.cwcommons.mock.MockListener;
+import com.crimsonwarpedcraft.cwcommons.mock.MockPlugin;
+import com.crimsonwarpedcraft.cwcommons.mock.MockPluginManager;
+import org.bukkit.event.Listener;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for ListenerRegister.
+ *
+ * @author Copyright (c) Levi Muniz. All Rights Reserved.
+ */
+class BukkitListenerRegisterTest {
+
+  @Test
+  void registerListener() {
+    MockPlugin plugin = new MockPlugin();
+    BukkitListenerRegister register = BukkitListenerRegister.getNewBukkitListenerRegister(plugin);
+
+    // Check NPE
+    assertThrows(NullPointerException.class, () -> register.registerListener(null));
+
+    // Register a mock listener with the register
+    Listener listener = new MockListener();
+    register.registerListener(listener);
+
+    // Check that the listener was registered successfully
+    assertTrue(
+        ((MockPluginManager) plugin
+            .getServer()
+            .getPluginManager())
+            .getListeners()
+            .contains(listener)
+    );
+  }
+
+  @Test
+  void getListenerRegister() {
+    // Check NPE
+    assertThrows(
+        NullPointerException.class,
+        () -> BukkitListenerRegister.getNewBukkitListenerRegister(null)
+    );
+  }
+}
